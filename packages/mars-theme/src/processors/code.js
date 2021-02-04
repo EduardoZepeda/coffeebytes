3d8655297a128025772b510896a14540df8e4c44
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-02-04 17:44:31 -0600

    Create a preprocessor for highlighting prism code
    The processor implements a prismProcessor object, this object test the
    component, it must be of type element and represent a code tag, its
    parent must be a pre tag and contain a class that starts with language-
    
    If this element passes all the texts then it we will modify its
    component property using the Code function in code.js. This function is
    in charge of calling the highlight method, to apply the token classes to
    the code. It will return the tokenized code inside a code with its
    respective class.

---
+++ packages/mars-theme/src/processors/code.js
@@ -0,0 +1,30 @@
+import { warn } from "frontity";
+import React from "react";
+
+import Prism from 'prismjs';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-docker';
+
+// Disable the automatic code highlighting of all `<code>` elements on the
+// client because we are doing it ourselves with a processors.
+Prism.manual = true;
+
+const Code = ({ className, children }) => {
+  // Capture the language of the snippet.
+  const result = className?.match(/language-(\w*)\b/);
+  try {
+    // Try to get highlight the snippet according to the syntax guessed from the
+    // `language-xxx` CSS class.
+    const language = result[1];
+    children = Prism.highlight(children, Prism.languages[language], language);
+  } catch (e) {
+    warn(
+      `An incorrect "${className}" CSS class has been specified on the <code> element so no syntax highlighting was applied. It could be because this language is not recognized by prism.js or because of a typo.`
+    );
+  }
+
+  return <code className={result?result[0]:null} dangerouslySetInnerHTML={{ __html: children }}></code>;
+};
+
+export default Code;
