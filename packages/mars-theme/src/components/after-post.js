ee804dba98f0f9e90603b37dc03da3e426c87e2f
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-02-20 20:15:46 -0600

    Remove twitter script and modify text from after-post

--- packages/mars-theme/src/components/after-post.js
+++ packages/mars-theme/src/components/after-post.js
@@ -1,15 +1,23 @@
-import { styled } from "frontity";
+import { styled, connect } from "frontity";
 
-const AfterPost = () => {
+const AfterPost = ({ state }) => {
 
     return (
         <>
             <h2>¿Quieres aprender más?</h2>
-            <p>Sígueme en Twitter y te aviso cada que tenga más contenido para ti</p>
-            <a href="https://twitter.com/neon_affogato?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-size="large" data-show-screen-name="false" data-lang="es" data-show-count="false">Follow @neon_affogato</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <p>Sígueme en Twitter y te aviso cuando tenga más contenido disponible para ti. ¡Es gratis!</p>
+            <FollowButton><a href={state.socialMedia.twitter} className="twitter-follow-button" data-size="large" data-show-screen-name="false" data-lang="es" data-show-count="false">Sí, llévame a Twitter</a></FollowButton>
         </>
         )
 
 };
 
-export default AfterPost;
+export default connect(AfterPost);
+
+
+const FollowButton = styled.button`
+    padding: 1rem 1.5rem;
+    background-color: #3370f3;
+    border: 0px solid;
+    border-radius: 4px;
+`;
