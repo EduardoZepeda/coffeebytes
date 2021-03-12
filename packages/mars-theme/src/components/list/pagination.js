8ca95dbc294793aaa9ee698160c982d861676932
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2021-03-11 20:18:30 -0600

    Replace simple pagination with numbered pagination
    Add numbered pagination to the bottom of the page. Also highlight the
    active page using conditional inline css styles.

--- packages/mars-theme/src/components/list/pagination.js
+++ packages/mars-theme/src/components/list/pagination.js
@@ -12,28 +12,25 @@ import Link from "../link";
  */
 const Pagination = ({ state, actions }) => {
   // Get the total posts to be displayed based for the current link
-  const { next, previous } = state.source.get(state.router.link);
-
+  const { next, previous, totalPages, page } = state.source.get(state.router.link);
   // Pre-fetch the the next page if it hasn't been fetched yet.
   useEffect(() => {
     if (next) actions.source.fetch(next);
   }, []);
-
   return (
     <div>
       {/* If there's a next page, render this link */}
-      {next && (
-        <Link link={next}>
-          <Text>← Publicaciones anteriores</Text>
-        </Link>
-      )}
-
-      {previous && next && " - "}
-
-      {/* If there's a previous page, render this link */}
       {previous && (
         <Link link={previous}>
-          <Text>Publicaciones recientes →</Text>
+          <PaginationLink>← Regresar </PaginationLink>
+        </Link>
+      )}
+      {totalPages && [...Array(totalPages).keys()].map(paginationPage=><Link id={paginationPage} link={`/page/${paginationPage+1}/`}><PaginationLink active={paginationPage+1===page}>{paginationPage+1}</PaginationLink></Link>)}
+
+      {/* If there's a previous page, render this link */}
+      {next && (
+        <Link link={next}>
+          <PaginationLink> Avanzar →</PaginationLink>
         </Link>
       )}
     </div>
@@ -49,4 +46,14 @@ export default connect(Pagination);
 const Text = styled.em`
   display: inline-block;
   margin-top: 16px;
+`;
+
+const PaginationLink = styled.div`
+  display: inline-block;
+  padding: 12px;
+  background-color: ${props => props.active? "#FFF": ""};
+  color: ${props => props.active? "#181818": "#FFF"};
+  border: 1px solid #FFF;
+  margin: 8px;
+  border-radius: 4px;
 `;
