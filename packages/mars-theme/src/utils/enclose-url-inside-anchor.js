0ade1ab1db28513fa090affaaadc942e653c73ed
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-03-09 10:49:45 -0600

    Fix regex pattern for urls
    The pattern used 09 instead of 0-9, producing errors in urls that
    contained numbers.

--- packages/mars-theme/src/utils/enclose-url-inside-anchor.js
+++ packages/mars-theme/src/utils/enclose-url-inside-anchor.js
@@ -1,5 +1,5 @@
 const encloseUrlInAnchorNoFollow = (text) => {
-  return text.replace(/(\b(https?|ftp|file|http):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z09+&@#/%=~_|])/img, '<a rel="nofollow" href="$1">$1</a>')
+  return text.replace(/(\b(https?|ftp|file|http):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/img, '<a rel="nofollow" href="$1">$1</a>')
 }
 
 export default encloseUrlInAnchorNoFollow
